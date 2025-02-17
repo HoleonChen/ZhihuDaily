acd163d0d52a43db63553b26532d5d463aa7cb09
Holeon <xsmaxczy@icloud.com>
2025-02-17 18:08:35 +0800

    [fix]修复了一些bug

--- Podfile
+++ Podfile
@@ -1,10 +1,11 @@
 source 'https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git'
 
-platform :ios,'11.0'
+platform :ios,'15.0'
 use_frameworks!
 
 target 'ZhihuDaily' do
   
   pod 'AFNetworking'
+  pod 'SDWebImage'
   
 end
--- ZhihuDaily/MainPage/View/MainPageTableViewCell.m
+++ ZhihuDaily/MainPage/View/MainPageTableViewCell.m
@@ -12,6 +12,7 @@
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{  //将各个元素添加到Cell中
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
+        self.backgroundColor = [UIColor whiteColor];
         [self.contentView addSubview:self.topicLabel];
         [self.contentView addSubview:self.hintLabel];
         [self.contentView addSubview:self.prevImageLabel];
@@ -28,12 +29,12 @@
 #pragma mark - Getter //懒加载
 
 - (UILabel *)topicLabel {  //标题懒加载
-    if (self == nil) {
+    if (_topicLabel == nil) {
         _topicLabel = [[UILabel alloc] init];
-        _topicLabel.frame = CGRectMake(45, 65, 810, 115);  //位置和大小
+        _topicLabel.frame = CGRectMake(15, 20, 270, 40);  //位置和大小
         _topicLabel.layer.masksToBounds = YES;  //超出边界限制
         _topicLabel.textColor = [UIColor blackColor];  //字体颜色黑色
-        _topicLabel.font = [UIFont systemFontOfSize:36];  //字号36磅
+        _topicLabel.font = [UIFont systemFontOfSize:16];  //字号18磅
         _topicLabel.numberOfLines = 2;  //限制行数2
         _topicLabel.textAlignment = NSTextAlignmentLeft;  //左对齐
     }
@@ -41,12 +42,12 @@
 }
 
 - (UILabel *)hintLabel {  //提示懒加载
-    if (self == nil) {
+    if (_hintLabel == nil) {
         _hintLabel = [[UILabel alloc] init];
-        _hintLabel.frame = CGRectMake(45, 205, 800, 40);  //位置和大小
+        _hintLabel.frame = CGRectMake(15, 65, 270, 13);  //位置和大小
         _hintLabel.layer.masksToBounds = YES;  //超出边界限制
         _hintLabel.textColor = [UIColor lightGrayColor];  //字体颜色亮灰色
-        _hintLabel.font = [UIFont systemFontOfSize:26];  //字号26磅
+        _hintLabel.font = [UIFont systemFontOfSize:10];  //字号8磅
         _hintLabel.numberOfLines = 1;  //限制行数1
         _hintLabel.textAlignment = NSTextAlignmentLeft;  //左对齐
     }
@@ -54,9 +55,9 @@
 }
 
 - (UIImageView *)prevImageLabel {
-    if (self == nil) {
+    if (_prevImageLabel == nil) {
         _prevImageLabel = [[UIImageView alloc] init];
-        _prevImageLabel.frame = CGRectMake(905, 45, 218, 218);  //位置和大小
+        _prevImageLabel.frame = CGRectMake(300, 14, 72, 72);  //位置和大小
         _prevImageLabel.layer.masksToBounds = YES;  //超出边界限制
         _prevImageLabel.layer.cornerRadius = 3;  //圆角半径3
     }
